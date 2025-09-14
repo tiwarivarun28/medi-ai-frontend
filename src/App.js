447b9a96316a67b9fd090019e4b914b652aad29a
tiwarivarun28 <117498408+tiwarivarun28@users.noreply.github.com>
2025-09-14 10:13:39 +0530

    basename

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
+  const basename =
+    process.env.NODE_ENV === "production" ? "/medi-ai-frontend" : "";
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <div className="app-shell">
         <Header />
         <Main>
