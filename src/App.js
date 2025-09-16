664eefe314db6f43e98c1e6b8c90846bf5995bf0
sudhanshu2710 <sudhanshu.singh2710@gmail.com>
2025-09-17 02:17:50 +0530

    loading added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 // import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
 // import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
+import LoadingPlane from "./components/layout/LoadingPlane";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   window.RDL = {};
   const basename =
     process.env.NODE_ENV === "production" ? "/medi-ai-frontend" : "/";
@@ -12,11 +15,17 @@ export default function App() {
   return (
     <BrowserRouter basename={basename}>
       <div className="app-shell">
-        {/* <Header /> */}
-        <Main>
-          <AppRoutes />
-        </Main>
-        {/* <Footer /> */}
+        {loading ? (
+          <LoadingPlane
+            onFinish={() => {
+              setLoading(false);
+            }}
+          />
+        ) : (
+          <Main>
+            <AppRoutes />
+          </Main>
+        )}
       </div>
     </BrowserRouter>
   );
